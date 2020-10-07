9bd973eba649fcb956a78f2c4785fcbc4451b55c
Freddy Rondon <fr4036@rit.edu>
2020-10-06 20:45:22 -0400

    Add project files.

---
+++ Data Structures/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Data_Structures
+{
+    class LinkedList<T>
+    {
+        private Node<T> head;
+
+        public LinkedList()
+        {
+            this.head = null;
+        }
+
+        // LIFO
+        public void InsertFirst(T value)
+        {
+            var current = new Node<T>(value);
+            if (this.head == null)
+            {
+                this.head = current;
+            }
+            else
+            {
+                var prev = this.head;
+                this.head = current;
+                this.head.Next = prev;
+            }
+        }
+
+        // FIFO
+        public void Append(T value)
+        {
+            var current = new Node<T>(value);
+            if (this.head == null)
+            {
+                this.head = current;
+            }
+            else
+            {
+                var runner = this.head;
+                while (runner != null)
+                {
+                    if (runner.Next == null)
+                    {
+                        runner.Next = current;
+                        break;
+                    }
+                    runner = runner.Next;
+                }
+
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new System.Text.StringBuilder();
+            var runner = this.head;
+            while (runner != null)
+            {
+               
+                sb.Append(runner.Value + " -> ");
+                runner = runner.Next;
+            }
+            sb.Append("END\n");
+            sb.Append("Head: " + this.head.Value);
+            return sb.ToString();
+        }
+    }
+}
---
+++ Data Structures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList<int>();
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+            System.Console.WriteLine("Append: " + list);
+
+            var list2 = new LinkedList<int>();
+            list2.InsertFirst(1);
+            list2.InsertFirst(2);
+            list2.InsertFirst(3);
+            System.Console.WriteLine("Insert first: " + list2);
+
+
+        }
+    }
+}
