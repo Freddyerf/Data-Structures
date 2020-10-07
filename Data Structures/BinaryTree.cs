75317bf982dc00876d2b19ac2d6ca0f0e86e1cf7
Freddy Rondon <frondonfermin@gmail.com>
2020-10-06 21:11:27 -0400

    Created Tree Node and Binary Tree structure

---
+++ Data Structures/BinaryTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures
+{
+    class BinaryTree<T>
+    {
+        private TreeNode<T> root;
+
+        public BinaryTree()
+        {
+            this.root = null;
+        }
+
+    }
+}
---
+++ Data Structures/TreeNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures
+{
+    class TreeNode<T>
+    {
+        public T Value { get; set; }
+
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+
+        public TreeNode()
+        {
+            this.Left = null;
+            this.Right = null;
+            this.Value = default;
+        }
+        public TreeNode(T value)
+        {
+            this.Left = null;
+            this.Right = null;
+            this.Value = value;
+        }
+        public TreeNode(T value, TreeNode<T> left, TreeNode<T> right)
+        {
+            this.Left = left;
+            this.Right = right;
+            this.Value = value;
+        }
+    }
+}
